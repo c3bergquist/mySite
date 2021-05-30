56d9318bf8aa2c7fd1ef84d1d0c9be3d04181cfe
Cory <cory3bergquist@gmail.com>
2021-05-30 19:51:53 -0400

    Restructring for firebase

--- functions/Gulpfile.js
+++ functions/Gulpfile.js
@@ -7,31 +7,31 @@ var gulp = require('gulp'),
 
 // Minify HTML
 gulp.task('html', () => {
-  return gulp.src('./src/html/*.html')
+  return gulp.src('../src/html/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./public/'));
 });
 
 // Compile Sass
 gulp.task('sass', function(){
-  return gulp.src('./src/sass/main.scss')
+  return gulp.src('../src/sass/main.scss')
     .pipe(sass())
 	.pipe(rename('style.css'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./public/dist'))
 });
 
 // Compile Js
 gulp.task('js', function(){
-  return gulp.src('./src/js/main.js')
+  return gulp.src('../src/js/main.js')
     .pipe(jsImport({hideConsole: true}))
 	.pipe(rename('script.js'))
 	.pipe(uglify())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./public/dist'))
 });
 
 // Watch task
 gulp.task('watch', function() {
-	gulp.watch('./src/html/*.html', gulp.series('html'));
-	gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-	gulp.watch('./src/js/**/*.js', gulp.series('js'));
+	gulp.watch('../src/html/*.html', gulp.series('html'));
+	gulp.watch('../src/sass/**/*.scss', gulp.series('sass'));
+	gulp.watch('../src/js/**/*.js', gulp.series('js'));
 });
