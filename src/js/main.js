c82ca4f2135fbc384824f7b44daa609b8919a294
Cory <cory3bergquist@gmail.com>
2021-05-31 02:26:45 -0400

    Responsive header, clickable nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-@import './src/js/super-mario-bros.js';
+var jsNavToggler = document.getElementById('js-nav-toggler'),
+	jsNavDrawer = document.getElementById('js-nav-drawer');
+
+jsNavToggler.addEventListener('click', function() {
+	if(jsNavDrawer.classList.contains('a-open-menu')) {
+		jsNavDrawer.classList.remove('a-open-menu');
+	} else {
+		jsNavDrawer.classList.add('a-open-menu');
+	}
+});
