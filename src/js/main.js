f77efdc27faef1c4135e95641f6d96ff5e556099
Cory <cory3bergquist@gmail.com>
2022-07-09 14:05:45 -0400

    Added overlay, header closes when toggling state

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ var $jsBody = $('#js-body'),
 	jsDarkModeToggler = document.getElementById('js-dark-mode-toggler'),
 	jsGamifyToggler = document.getElementById('js-gamify-toggler'),
 	jsNavToggler = document.getElementById('js-nav-toggler'),
-	jsNavDrawer = document.getElementById('js-nav-drawer');
+	jsNavDrawer = document.getElementById('js-nav-drawer'),
+	jsNavOverlay = document.getElementById('js-nav-overlay');
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	var isDarkMode = sessionStorage.getItem('isDarkMode');
@@ -29,6 +30,8 @@ jsDarkModeToggler.addEventListener('click', function() {
 		$jsBody[0].classList.add('is-dark-mode');
 		sessionStorage.setItem('isDarkMode', true);
 	}
+
+	toggleNav();
 });
 
 jsGamifyToggler.addEventListener('click', function() {
@@ -44,12 +47,19 @@ jsGamifyToggler.addEventListener('click', function() {
 		$jsBody[0].classList.add('is-gamified');
 		sessionStorage.setItem('isGamified', true);
 	}
+
+	toggleNav();
 });
 
-jsNavToggler.addEventListener('click', function() {
+jsNavToggler.addEventListener('click', function() { toggleNav(); });
+jsNavOverlay.addEventListener('click', function() { toggleNav(); });
+
+function toggleNav() {
 	if(jsNavDrawer.classList.contains('a-open-menu')) {
 		jsNavDrawer.classList.remove('a-open-menu');
+		jsNavOverlay.classList.remove('a-open-menu');
 	} else {
 		jsNavDrawer.classList.add('a-open-menu');
+		jsNavOverlay.classList.add('a-open-menu');
 	}
-});
+}
